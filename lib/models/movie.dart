be47c41d8eeccbeb6386c5ae89e86fc23a4899b6
JGonzalez118 <josue08gl@gmail.com>
2025-07-02 20:18:48 -0500

    Series Fix

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -19,14 +19,19 @@ class Movie {
 
 
   factory Movie.fromJson(Map<String, dynamic> json) {
+
+    final String title = json['title'] ?? json ['name'] ?? 'Titulo Desconocido'; 
+    final String releaseDate = json['release_date'] ?? json['first_air_date'] ?? 'Fecha Desconocida';
+
+
     return Movie(
       id: json['id'].toString(), // Asegura que el ID sea String
-      title: json['title'] ?? 'Título Desconocido', // Proporciona un valor por defecto si es nulo
+      title: title, // Proporciona un valor por defecto si es nulo
       posterPath: json['poster_path'] ?? '', // Ruta del póster, vacía si es nula
       backdropPath: json['backdrop_path'], // Ruta del fondo, puede ser nula
       overview: json['overview'] ?? 'Sin descripción.', // Descripción, valor por defecto si es nula
       voteAverage: (json['vote_average'] as num?)?.toDouble() ?? 0.0, // Calificación, 0.0 por defecto
-      releaseDate: json['release_date'] ?? 'Fecha Desconocida', // Fecha de estreno, valor por defecto
+      releaseDate: releaseDate // Fecha de estreno, valor por defecto
     );
   }
 }
